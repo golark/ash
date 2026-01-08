8136052c45a3f8ad84b17d5d1626538dc49e1af1
can <firatcnur@gmail.com>
2026-01-08 20:46:57 +0000

    added widget

---
+++ widget.zsh
@@ -0,0 +1,9 @@
+# Example: Insert text at cursor position
+zle -N insert-semicolon
+insert-semicolon() {
+    local input="${LBUFFER}${RBUFFER}"
+    LBUFFER=""
+    RBUFFER=""
+    RBUFFER="$(./build/cli "$input")"
+}
+bindkey '^G' insert-semicolon
